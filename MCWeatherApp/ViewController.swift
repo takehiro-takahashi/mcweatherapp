a563d03d6e5bc3ead24a49bb8c5e5023cb3cdd78
takehiro <takehiro@kakusin.co.jp>
2017-04-14 23:21:27 +0900

    チェックボタンが押された際の挙動を作成

--- MCWeatherApp/ViewController.swift
+++ MCWeatherApp/ViewController.swift
@@ -19,11 +19,16 @@ class ViewController: UIViewController {
     // チェックボタン
     @IBOutlet weak var checkButton: UIButton!
     
+    // APIリクエストや、レスポンスデータを利用するためのクラスインスタンス
+    let dataManager = WeatherDataManager()
+    
 
+    // 画面が表示されたときの処理
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        // ここでAPIリクエストを行う
+        self.dataManager.dataRequest()
         
         
     }
@@ -36,6 +41,22 @@ class ViewController: UIViewController {
     
     // ここでチェックボタンを押して離したときの挙動
     @IBAction func checkButtonTapped(_ sender: Any) {
+        // お天気API 返却値によって画像を変更する条件式
+        if dataManager.weatherData?.weather == "Clouds" {
+            // 「Clouds」だったら「曇り」 画像を表示する
+            weatherImage.image = UIImage(named: "cloud")
+        } else if dataManager.weatherData?.weather == "Clear" {
+            // 「Clear」だったら「晴れ」 画像を表示する 
+            weatherImage.image = UIImage(named: "sunny")
+        } else if dataManager.weatherData?.weather == "Rain" {
+            // 「Rain」だったら「雨」 画像を表示する
+            weatherImage.image = UIImage(named: "rain")
+        }
+        
+        // 気温 ラベルに取得した気温を表示させる
+        tempLabel.text = dataManager.weatherData?.temp.description
+        // ボタンを隠す
+        self.checkButton.isHidden = true
     }
     
     
